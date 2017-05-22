a296977885e4e03c1c7b6074c149f56c7329f402
fedefom <fedeomartinez@gmail.com>
2017-05-22 19:52:22 -0300

    Muestre de articulos segun seccion seleccionada en desplegable
    Se muestran los articulos que se corresponden con la seccion selecionada en el desplegable.
    
    Se comento el codigo para una mejor lectura

--- www/js/welcome.js
+++ www/js/welcome.js
@@ -57,8 +57,62 @@ function MostrarSecciones(resp) {
     var Secciones = resp.split(";");
     for (var i = 0, len = Secciones.length; i < len; i++) {
         if (Secciones[i]!="")
-            elem.innerHTML += '<div><a href="#"><label>' + Secciones[i] + '</label></a></div>';
+            elem.innerHTML += '<div onclick="MostrarListadoArticulosSegunSeccion(\'' + Secciones[i].trim() + '\')"><a><label>' + Secciones[i] + '</label></a></div>';
     }
     elem.innerHTML += '<h3>Opciones</h3>';
+
+}
+
+//Cuando se selecciona una seccion del menu desplegable, aca se obtiene los articulos relacionados a dicha seccion para mostrarla en el ContenedorPrincipal
+function MostrarListadoArticulosSegunSeccion(Seccion) {
+
+    var access_token = 'hD6ZEfkGwbAAAAAAAAAAB64YKvNF4qCgA026Y9mqceeaE4jdtPcFAL_vCZZU4zmy';
+    var request = new XMLHttpRequest();
+    var datos = JSON.stringify({
+        'path': "/Articulos",
+
+    });
+
+    request.open('POST', 'https://api.dropboxapi.com/2/files/list_folder', false);
+    request.setRequestHeader('Authorization', 'Bearer ' + access_token);
+    request.setRequestHeader('Content-Type', 'application/json');
+
+    request.onload = function () {
+        if (request.status >= 200 && request.status < 400) {
+            //Limpio el contenedor principal
+            $("#ContenidoPrincipal").html("");
+            var Contenedor = document.getElementById("ContenidoPrincipal");
+            Contenedor.innerHTML += "<div><H5>Articulos acerca de " + Seccion + "</H5></div>";
+            var resp = JSON.parse(request.responseText);
+            //proceso el nombre de cada articulo de la categoria seleccionada
+            var HayContenido = false;
+            //proceso los articulos
+            for (var i = 0, len = resp.entries.length; i < len; i++) {
+                //verifico si el articulo es correspondiente a la seccion seleccionada
+                if (resp.entries[i].name.indexOf(Seccion) != -1) { 
+                    var nombreArticulo = resp.entries[i].name.substring(resp.entries[i].name.lastIndexOf("-") +1).replace(".txt","");
+                    Contenedor.innerHTML += "<div><p>" + nombreArticulo + "</p></div>";
+                    HayContenido = true;
+                }
+            }
+            //Verifico si se encontraron articulos, en caso contrario lo notifico
+            if (!HayContenido) {
+                Contenedor.innerHTML += "<div><p>No hay articulos relacionados</p></div>";
+            }
+            //minimizo el menu desplegable
+            closeNav();
+        } else {
+            // En caso de error
+            console.log('No se pudo leer el archivo');
+        }
+    };
+
+    request.onerror = function () {
+        console.log('No existe el archivo');
+    };
+
+    request.send(datos);
+
+
 
 }
