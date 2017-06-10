4364ca88945a190c2d31077d285a49246cf256c0
facuceri <facundo.proyectois@gmail.com>
2017-06-09 21:53:00 -0300

    Bugs corregidos y vista de consulta

--- www/js/index.js
+++ www/js/index.js
@@ -57,25 +57,6 @@ function signOut() {
     window.location.replace("index.html");
 }
 
-
-//=======
-//        // TODO: Cordova se ha cargado. Haga aquí las inicializaciones que necesiten Cordova.
-//        var parentElement = document.getElementById('deviceready');
-//        var listeningElement = parentElement.querySelector('.listening');
-//        var receivedElement = parentElement.querySelector('.received');
-//        listeningElement.setAttribute('style', 'display:none;');
-//        receivedElement.setAttribute('style', 'display:block;');
-//    };
-
-//    function onPause() {
-//        // TODO: esta aplicación se ha suspendido. Guarde el estado de la aplicación aquí.
-//    };
-
-//    function onResume() {
-//        // TODO: esta aplicación se ha reactivado. Restaure el estado de la aplicación aquí.
-//    };
-//} )();
-
 function abrirModal() {
     activar();
     $(document).ready(function () {
@@ -104,7 +85,6 @@ function desactivar() {
     document.getElementById("provincia").disabled = true;
 }
 
-
 function elegirPais() {
     var pais = document.getElementById("pais").value;
     var provincia = document.getElementById("provincia");
@@ -114,7 +94,6 @@ function elegirPais() {
         provincia.style.display = "none";
     }
 }
-
 
 function validar() {
     var edad = document.getElementById("edad").value;
@@ -157,7 +136,7 @@ function validar() {
         validarPais = true;
     }
 
-    if (validarEdad == true && validarGenero == true && validarPais && true) {
+    if (validarEdad == true && validarGenero == true && validarPais == true) {
         if (pais == "Argentina") {
             if (validarProvincia == true) {
                 insertDB();
@@ -178,68 +157,6 @@ function cargarEventos() {
     document.getElementById("modalBtn").addEventListener('click', searchDB);
 } 
 
-
-
-// Crea laa table en la base de datos si no existe
-function populateDB(tx) {
-    tx.executeSql('CREATE TABLE IF NOT EXISTS USUARIO (id INTEGER PRIMARY KEY AUTOINCREMENT, edad, genero, pais, provincia)');
-}
-
-// Busca en la tabla de la base de datos
-function queryDB(tx) {
-    tx.executeSql('SELECT * FROM USUARIO', [], querySuccess);
-}
-
-// Se ejecuta si busco con exito en la base de datos
-//
-function querySuccess(tx, results) {
-            
-    if (results.rows.length == 0) { // Verifica si obtuvo resultado
-        abrirModal(); // Abre el modal para el registro anonimo
-    }else{
-        window.location.href = "welcome.html"; // Manda directamente al welcome.html
-    }
-            
-}
-
-// Transaction success callback
-//
-function successCB() {
-     alert("success!");
-}
-
-// Transaction error callback
-//
-function errorCB(err) {
-    alert("Error processing SQL: "+err.code);
-}
-
-//function createDB() {
-//    var db = window.openDatabase("Psicopedia", "1.0", "Psicopedia Demo", 200000);
-//    db.transaction(populateDB, errorCB, successCB);
-//}
-
-// Inserta en la base de datos
-function insertSql(tx) {
-    var edad = document.getElementById("edad").value;
-    var genero = document.getElementById("genero").value;
-    var pais = document.getElementById("pais").value;
-    var provincia = document.getElementById("provincia").value;
-    
-    tx.executeSql('INSERT INTO USUARIO (edad,genero,pais,provincia) VALUES ("' + edad
-                    + '","' + genero + '","' + pais + '","' + provincia + '")');
-}
-
-function insertDB() {
-    var db = window.openDatabase("Psicopedia", "1.0", "Psicopedia Demo", 200000);
-    db.transaction(insertSql); //Inserta en la tabla
-}
-
-function searchDB() {
-    var db = window.openDatabase("Psicopedia", "1.0", "Psicopedia Demo", 200000); // Devuelve una instancia de la base de datos
-    db.transaction(populateDB); //Crea la tabla
-    db.transaction(queryDB); // Busca en la tabla
-}
 
 
 
