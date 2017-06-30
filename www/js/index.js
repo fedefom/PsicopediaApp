525a1ae41690cab03a79c12a1980308e58a76a96
facuceri <facundo.proyectois@gmail.com>
2017-06-30 17:04:23 -0300

    Cambios de estilo

--- www/js/index.js
+++ www/js/index.js
@@ -128,36 +128,20 @@ function cerrarModal() {
 function activar() {
     document.getElementById("edad").disabled = false;
     document.getElementById("genero").disabled = false;
-    //document.getElementById("pais").disabled = false;
-    //document.getElementById("provincia").disabled = false;
 }
 
 function desactivar() {
     document.getElementById("edad").disabled = true;
     document.getElementById("genero").disabled = true;
-    //document.getElementById("pais").disabled = true;
-    //document.getElementById("provincia").disabled = true;
 }
 
-//function elegirPais() {
-//    var pais = document.getElementById("pais").value;
-//    var provincia = document.getElementById("provincia");
-//    if (pais == "Argentina") {
-//        provincia.style.display = "block";
-//    } else {
-//        provincia.style.display = "none";
-//    }
-//}
 
 function validar() {
     var edad = document.getElementById("edad").value;
     var genero = document.getElementById("genero").value;
-    //var pais = document.getElementById("pais").value;
-    //var provincia = document.getElementById("provincia").value;
     var validarEdad;
     var validarGenero;
-    //var validarPais;
-    //var validarProvincia;
+
 
     if (edad == "Edad") {
         document.getElementById("edadMsj").innerText = "Debe completar la edad";
@@ -194,89 +178,89 @@ function validar() {
     }
 }
 
-// This is called with the results from from FB.getLoginStatus().
-function statusChangeCallback(response) {
-    console.log('statusChangeCallback');
-    console.log(response);
-    // The response object is returned with a status field that lets the
-    // app know the current login status of the person.
-    // Full docs on the response object can be found in the documentation
-    // for FB.getLoginStatus().
-    var page = document.getElementById("pagina").innerText;
-    console.log(page);
-    if (response.status === 'connected') {
-        // Logged into your app and Facebook.
-        window.location.href = page;
-        //testAPI();
-    } else {
-        page = document.getElementById("pagina").innerText = "completar.html";
-        console.log(page);
-    }
-}
+//// This is called with the results from from FB.getLoginStatus().
+//function statusChangeCallback(response) {
+//    console.log('statusChangeCallback');
+//    console.log(response);
+//    // The response object is returned with a status field that lets the
+//    // app know the current login status of the person.
+//    // Full docs on the response object can be found in the documentation
+//    // for FB.getLoginStatus().
+//    var page = document.getElementById("pagina").innerText;
+//    console.log(page);
+//    if (response.status === 'connected') {
+//        // Logged into your app and Facebook.
+//        window.location.href = page;
+//        //testAPI();
+//    } else {
+//        page = document.getElementById("pagina").innerText = "completar.html";
+//        console.log(page);
+//    }
+//}
 
-// This function is called when someone finishes with the Login
-// Button.  See the onlogin handler attached to it in the sample
-// code below.
-function checkLoginState() {
-    FB.getLoginStatus(function (response) {
-        statusChangeCallback(response);
-    });
-}
+//// This function is called when someone finishes with the Login
+//// Button.  See the onlogin handler attached to it in the sample
+//// code below.
+//function checkLoginState() {
+//    FB.getLoginStatus(function (response) {
+//        statusChangeCallback(response);
+//    });
+//}
 
-window.fbAsyncInit = function () {
-    FB.init({
-        appId: '121460021782812',
-        status: true,
-        cookie: true,  // enable cookies to allow the server to access the session
-        xfbml: true,  // parse social plugins on this page
-        version: 'v2.9' // use graph api version 2.8
-    });
+//window.fbAsyncInit = function () {
+//    FB.init({
+//        appId: '121460021782812',
+//        status: true,
+//        cookie: true,  // enable cookies to allow the server to access the session
+//        xfbml: true,  // parse social plugins on this page
+//        version: 'v2.9' // use graph api version 2.8
+//    });
 
-    // Now that we've initialized the JavaScript SDK, we call
-    // FB.getLoginStatus().  This function gets the state of the
-    // person visiting this page and can return one of three states to
-    // the callback you provide.  They can be:
-    //
-    // 1. Logged into your app ('connected')
-    // 2. Logged into Facebook, but not your app ('not_authorized')
-    // 3. Not logged into Facebook and can't tell if they are logged into
-    //    your app or not.
-    //
-    // These three cases are handled in the callback function.
+//    // Now that we've initialized the JavaScript SDK, we call
+//    // FB.getLoginStatus().  This function gets the state of the
+//    // person visiting this page and can return one of three states to
+//    // the callback you provide.  They can be:
+//    //
+//    // 1. Logged into your app ('connected')
+//    // 2. Logged into Facebook, but not your app ('not_authorized')
+//    // 3. Not logged into Facebook and can't tell if they are logged into
+//    //    your app or not.
+//    //
+//    // These three cases are handled in the callback function.
 
-    FB.getLoginStatus(function (response) {
-        statusChangeCallback(response);
-    });
+//    FB.getLoginStatus(function (response) {
+//        statusChangeCallback(response);
+//    });
 
-};
+//};
 
-// Here we run a very simple test of the Graph API after login is
-// successful.  See statusChangeCallback() for when this call is made.
-function testAPI() {
-    console.log('Welcome!  Fetching your information.... ');
-    FB.api('/me', { fields: 'name,email,age_range' }, function (response) {
-        console.log(response);
-        console.log('Successful login for: ' + response.name);
-        console.log('Successful login for: ' + response.email);
-        console.log('Successful login for: ' + response.age_range.min);
+//// Here we run a very simple test of the Graph API after login is
+//// successful.  See statusChangeCallback() for when this call is made.
+//function testAPI() {
+//    console.log('Welcome!  Fetching your information.... ');
+//    FB.api('/me', { fields: 'name,email,age_range' }, function (response) {
+//        console.log(response);
+//        console.log('Successful login for: ' + response.name);
+//        console.log('Successful login for: ' + response.email);
+//        console.log('Successful login for: ' + response.age_range.min);
         
-        //FEDE COMPLETAR PARA QUE GRABE EL RANGO DE EDAD EN EL GA
-        window.location.href = "welcome.html";
-    });
-}
+//        //FEDE COMPLETAR PARA QUE GRABE EL RANGO DE EDAD EN EL GA
+//        window.location.href = "welcome.html";
+//    });
+//}
 
-function facebookLogin() {
-    FB.login(function (response) {
-        if (response.status === 'connected') {
-            // Logged into your app and Facebook.
-            var page = document.getElementById("pagina").innerText;
-            console.log(page);
-            window.location.href = page;
-        } else {
-            // The person is not logged into this app or we are unable to tell. 
-        }
-    }, { scope: 'public_profile,email' });
-}
+//function facebookLogin() {
+//    FB.login(function (response) {
+//        if (response.status === 'connected') {
+//            // Logged into your app and Facebook.
+//            var page = document.getElementById("pagina").innerText;
+//            console.log(page);
+//            window.location.href = page;
+//        } else {
+//            // The person is not logged into this app or we are unable to tell. 
+//        }
+//    }, { scope: 'public_profile,email' });
+//}
 
 
 
